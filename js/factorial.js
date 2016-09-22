9a5d3437befab9465147fd92393c315e8e6ec49c
agdavid <antonio.g.david@gmail.com>
2016-09-22 08:47:03 -0400

    refactor factorial() function to use recursion

--- js/factorial.js
+++ js/factorial.js
@@ -1,18 +1,30 @@
-function Factorial(number) {
+function factorial(number) {
 
-  //using loop, without recursion
-  var result = 1;
+  // //using loop, without recursion
+  // var result = 1;
 
-  for (var count = number; count > 1; count--) {
-    //print expression to confirm multiplied values
-    console.log(result.toString() + ' * ' + count.toString());
-    //multiply
-    result *= count;
-    //print result
-    console.log(result.toString());
+  // for (var count = number; count > 1; count--) {
+  //   //print expression to confirm multiplied values
+  //   console.log(result.toString() + ' * ' + count.toString());
+  //   //multiply
+  //   result *= count;
+  //   //print result
+  //   console.log(result.toString());
+  // }
+
+  // alert("The factorial of " + number.toString() + " is: " + result.toString());
+
+  // //note: the drawback of this loop is the need for local variables
+
+  //using recursion
+  if (number <= 0) {
+    return 1;
+  } else {
+    return number * factorial(number-1);
   }
-
-  alert("The factorial of " + number.toString() + " is: " + result.toString());
-
-  //note: the drawback of this loop is the need for local variables
+    //if factorial(3)
+      //first loop => 3 * factorial(2)
+      //second loop => 3 * 2 * factorial(1)
+      //third loop => 3 * 2 * 1 * factorial(0)
+      //return => 3 * 2 * 1 * 1
 }
